3bbd0dc279bc7494f5cc9a5b4bc043c40b4ce0f8
shagi09 <shalomwubu024@gmail.com>
2024-07-06 16:14:03 +0300

    mongodb connected

---
+++ pizzas.php
@@ -0,0 +1,14 @@
+<?php
+require "vendor/autoload.php";
+$client = new \MongoDB\Client("mongodb://localhost:27017");
+$pizzadb = $client->pizzadb;
+$result1 = $pizzadb->createCollection('pizzacollection');
+var_dump($result1);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include('layout/header.php')?>
+
+<?php include('layout/footer.php')?>
+</html>
