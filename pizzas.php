c714d3a04fb702d3e6e3b57e171753d2facc05c3
shagi09 <shalomwubu024@gmail.com>
2024-07-09 22:23:11 +0300

    data retrieved

--- pizzas.php
+++ pizzas.php
@@ -7,17 +7,29 @@ require "vendor/autoload.php";
 $client = new \MongoDB\Client("mongodb://localhost:27017");
 $pizzadb = $client->pizzadb;
 $result1 = $pizzadb->createCollection('pizzacollection');
+$db=$client->selectDatabase("pizzadb");
+$collection=$db->selectCollection("pizzacollection");
 var_dump($result1);
+$result=$collection->find();
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <?php include('layout/header.php')?>
 <h2 style='margin-left:590px;'>your orders are:</h2>
-<div style='margin-left:450px;width: 400px;background-color:#D1B89E; padding:40px; border-radius:20px;color:white;font-size:30px;'>
-    <div>title:</div>
-    <div>ingredients:</div>
-</div>
+<?php
+foreach($result as $doc){
+    echo "<div  style='margin-top:20px;margin-left:450px;width: 400px;background-color:#D1B89E; padding:40px; border-radius:20px;color:white;font-size:30px;'>";
+    echo "<div>title:{$doc['title']}</div>";
+    echo "<div>ingredients:{$doc['ingredients']}</div>";
+    echo "</div>";
+
+
+}
+
+?>
+
+
 
 <?php include('layout/footer.php')?>
 </html>
